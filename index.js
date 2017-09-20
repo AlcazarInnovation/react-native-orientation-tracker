9746fe5f22a819b5e5018d5e2de26ee3e8c2fc2d
Alcazar, Carlos A <Carlos.Alcazar@caresource.com>
2017-09-20 09:21:50 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import { Dimensions } from 'react-native';
+
+export let orientation;
+export default class tracker {
+    constructor() {
+        this.layoutChange = this.layoutChange.bind(this);
+    }
+    init() {
+        this.layoutChange();
+    }
+    layoutChange() {
+        let {width, height} = Dimensions.get('window');
+        this.setOrientation(width, height)
+    }
+    setOrientation(width, height) {
+        if (width > height) {
+            orientation = "landscape";
+        }
+
+        if (width < height) {
+            orientation = "portrait";
+        }
+
+        if (width === height) {
+            orientation = "square";
+        }
+    }
+}
+
+
+
+
